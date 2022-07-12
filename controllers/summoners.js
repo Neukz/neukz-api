6c51a8b39ad5bc2b7f7a96eeaf993e4dd39fb960
Kacper Neumann <kapilek31@gmail.com>
2022-07-12 17:36:59 +0200

    Created controller for Summoners route

---
+++ controllers/summoners.js
@@ -0,0 +1,17 @@
+const { riotLOL } = require('../utils/axios');
+
+// Get summoner account data and League of Legends stats from Riot Games API
+exports.getSummoner = async function (req, res, next) {
+	const { region, summonerName } = req.params;
+
+	const summoner = await riotLOL.get(
+		`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
+	);
+
+	const stats = await riotLOL.get(
+		`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.data.id}`
+	);
+
+	const response = { summoner: summoner.data, stats: stats.data };
+	res.status(200).json(response);
+};
