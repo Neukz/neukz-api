e1b69ce1b806533c2fc0161a41e127e155d5ffe7
Kacper Neumann <kapilek31@gmail.com>
2022-07-13 11:59:38 +0200

    Renamed axios instances

--- controllers/summoners.js
+++ controllers/summoners.js
@@ -1,14 +1,14 @@
-const { riotLOL } = require('../utils/axios');
+const { LeagueOfLegendsAPI } = require('../utils/axios');
 
 // Get summoner account data and League of Legends stats from Riot Games API
 exports.getSummoner = async function (req, res, next) {
 	const { region, summonerName } = req.params;
 
-	const summoner = await riotLOL.get(
+	const summoner = await LeagueOfLegendsAPI.get(
 		`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
 	);
 
-	const stats = await riotLOL.get(
+	const stats = await LeagueOfLegendsAPI.get(
 		`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.data.id}`
 	);
 
