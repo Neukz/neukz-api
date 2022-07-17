48fd8d8df7782dad087467671c3d028057b852b1
Kacper Neumann <kapilek31@gmail.com>
2022-07-17 23:58:09 +0200

    Concurrent requests for LOL and TFT data with filtering

---
+++ constants/riotResponseFields.js
@@ -0,0 +1,14 @@
+// Significant response fields from Riot Games API - the rest will be filtered out
+
+// Summoner fields
+exports.summonerFields = ['name', 'profileIconId', 'summonerLevel'];
+
+// Stats fields
+exports.statsFields = [
+	'queueType',
+	'tier',
+	'rank',
+	'leaguePoints',
+	'wins',
+	'losses'
+];
--- controllers/summoners.js
+++ controllers/summoners.js
@@ -1,17 +1,46 @@
-const { LeagueOfLegendsAPI } = require('../utils/axios');
+const { LeagueOfLegendsAPI, TeamfightTacticsAPI } = require('../utils/axios');
+const {
+	summonerFields,
+	statsFields
+} = require('../constants/riotResponseFields');
 
 // Get summoner account data and League of Legends stats from Riot Games API
 exports.getSummoner = async function (req, res, next) {
 	const { region, summonerName } = req.params;
 
+	// Get summoner account data
 	const summoner = await LeagueOfLegendsAPI.get(
 		`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
 	);
 
-	const stats = await LeagueOfLegendsAPI.get(
-		`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.data.id}`
-	);
+	// Get summoner stats from both games simultaneously
+	const stats = await Promise.all([
+		LeagueOfLegendsAPI.get(
+			`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.data.id}`
+		),
+		TeamfightTacticsAPI.get(
+			`https://${region}.api.riotgames.com/tft/league/v1/entries/by-summoner/${summoner.data.id}`
+		)
+	]);
 
-	const response = { summoner: summoner.data, stats: stats.data };
+	// Filter out inisignificant fields from summoner object
+	const filteredSummoner = {};
+	summonerFields.map(field => {
+		filteredSummoner[field] = summoner.data[field];
+	});
+
+	// Filter out inisignificant fields from stats response
+	const filteredStats = stats.map(game => {
+		return game.data.map(stats => {
+			const filteredQueue = {};
+			statsFields.map(field => {
+				filteredQueue[field] = stats[field];
+			});
+			return filteredQueue;
+		});
+	});
+
+	const [LOL, TFT] = filteredStats;
+	const response = { summoner: filteredSummoner, stats: { LOL, TFT } };
 	res.status(200).json(response);
 };
