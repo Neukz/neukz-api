cdd827236ec9e5c4c81c88ea9d815b8560a71e7d
Kacper Neumann <kapilek31@gmail.com>
2022-08-05 22:52:42 +0200

    Removed filtering summoner object

--- constants/riot/responseFields.js
+++ constants/riot/responseFields.js
@@ -1,14 +1,5 @@
 // Significant response fields from Riot Games API - the rest will be filtered out
 
-// Summoner fields
-exports.summonerFields = [
-	'id',
-	'puuid',
-	'name',
-	'profileIconId',
-	'summonerLevel'
-];
-
 // Stats fields
 exports.statsFields = [
 	'queueType',
